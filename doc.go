6014b00df3b4c1dd602c7bfa3637b5c9368019bc
Yuki Ito <mrno110y@gmail.com>
2022-11-03 15:38:52 +0900

    doc: add doc.go

---
+++ doc.go
@@ -0,0 +1,4 @@
+/*
+`gopubsub` is a generic [topic-based Publish–Subscribe](https://en.wikipedia.org/wiki/Publish%E2%80%93subscribe_pattern) library for Go backed by Goroutines and Channels.
+*/
+package gopubsub
